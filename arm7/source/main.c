699eecb4e0b1d6061a85f2dfb77c95060f9f0739
ApacheThunder <spenserwhaley@gmail.com>
2016-08-20 09:24:03 -0500

    More indent fixes. :P

--- arm7/source/main.c
+++ arm7/source/main.c
@@ -107,16 +107,16 @@ int main(void) {
 	ResetSlot();
 	
 	// Keep the ARM7 mostly idle
-		while (1) {
+	while (1) {
 
 			if(*((vu32*)0x027FFE24) == (u32)0x027FFE04)
 			{
 				irqDisable (IRQ_ALL);
 				*((vu32*)0x027FFE34) = (u32)0x06000000;
-
 			}
-				swiSoftReset();
-			} 
-			swiWaitForVBlank();
+			swiSoftReset();
+			}
+
+	swiWaitForVBlank();
 }
 
